145644d4ce00eced799b92949dc7f3be3aeb8e68
Daniel Wu <danielwu.toucan@gmail.com>
2024-04-21 00:48:42 -0400

    [Fix] opengraph and twitter card fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import Head from "next/head";
 export const metadata = {
   title: "Daniel Wu",
   description: "Daniel Wu's personal website",
+  openGraph: {
+    images: ["/images/toucanfish.jpeg"],
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +18,14 @@ export default function RootLayout({
       <Head>
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="Daniel Wu" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.openGraph.images[0]} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.openGraph.images[0]} />
       </Head>
       <body>{children}</body>
     </html>
